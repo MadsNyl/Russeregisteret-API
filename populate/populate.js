58a78439916c45a34f9d47385c193dfbe2693f4f
Mads Nylund <madsnyl@gmail.com>
2022-11-16 12:01:15 +0100

    changes credentials

--- populate/populate.js
+++ populate/populate.js
@@ -1,12 +1,17 @@
 const reader = require("xlsx");
 const mysql = require("mysql2");
+require("dotenv").config();
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB = process.env.DB;
 
 const pool = mysql.createConnection({
     connectionLimit: 5,
-    host: "mysql-ait.stud.idi.ntnu.no",
-    user: "mknylund",
-    password: "Rm2.II4DW",
-    database: "mknylund",
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB,
     multipleStatements: true
 });
 
